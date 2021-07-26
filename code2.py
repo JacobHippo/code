5367d2966f6a312a8bb6372600f4be082ed9cd23
Jcob Hippo <s910236@mail.hwsh.tc.edu.tw>
2021-07-26 11:34:17 +0800

    teacher commit

---
+++ code2.py
@@ -0,0 +1,11 @@
+password = 'a123456'
+x = 3
+while True:
+	code = input('請輸入密碼')
+	if code == password:
+		print('登入成功')
+	else:
+		x = x-1
+		print('密碼錯誤還剩', x, '次機會')
+        if x == 0:
+        	break
